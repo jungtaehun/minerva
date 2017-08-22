fd15f546a18f9a33d9e3629d53aab7bc9cee1c7e
BlueBada <wxg1297@naver.com>
2017-08-22 01:20:04 +0000

    course_ajax

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -26,35 +26,35 @@ class CoursesController < ApplicationController
       end
 end
   def favorites_add
-    #@user = User.find(params[:u_id])
-    #@course = Course.find(params[:c_id])
-     @array=Array.new
-     @array << "1"
-     @array << "2"
+    user = User.find(params[:u_id])
+    course = Course.find(params[:c_id])
+    
      
-    #if user.favorites_addition(user.id, course.id)
+    if user.favorites_addition(user.id, course.id)
       respond_to do |format|
-        format.js { render :json => "true" }
+        format.js { render :json => true }
+      end
+    else
+      respond_to do |format|
+        format.js { render :json => false }
+      end
     end
-    #else
-    #  respond_to do |format|
-    #    format.js { render :json => @result }
-    #end
-    #end
   end
 
   def favorites_delete
-    user = Student.find(params[:u_id])
+    user = User.find(params[:u_id])
     course = Course.find(params[:c_id])
     
-    #if user.favorites_deletion(user.id, course.id)
+     
+    if user.favorites_deletion(user.id, course.id)
       respond_to do |format|
-        format.js { render :json => "week"}
+        format.js { render :json => true }
+      end
+    else
+      respond_to do |format|
+        format.js { render :json => false }
+      end
     end
-    #else
-    #  respond_to do |format|
-    #    format.js { render :json => @result }
-    #end
-    #end
+    
   end
 end
